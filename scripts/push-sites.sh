7ab4d8bd12d28e494505f8cf6ea7d3e12158a220
anatolianserdar <173465918+anatolianserdar@users.noreply.github.com>
2025-06-11 15:02:09 +0300

    push sh updated

--- scripts/push-sites.sh
+++ scripts/push-sites.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
 # git submodule foreach 'git checkout main'
 # git submodule foreach 'git pull'
 
 git submodule foreach 'git add . && git commit -m "Sites updated"'
+git submodule foreach 'git push'
+
 git add .
 git commit -m "Update submodule references"
-git submodule foreach 'git push'
 git push
