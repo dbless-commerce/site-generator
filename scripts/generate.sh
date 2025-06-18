bbe89dbcbc9ec94e4978bf239deb668d55fea6c4
ahmedjw <ahmedvander@gmail.com>
2025-06-18 23:31:58 +0300

    footer styling fixs

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bash scripts/helpers/create-css.
+bash scripts/helpers/create-css.sh
 bash scripts/helpers/create-products.sh
 bash scripts/helpers/copy-images.sh
 bash scripts/helpers/create-pages.sh
