0f9554db0d876ccaf996626ab03307269245bdd2
ahmedjw <ahmedvander@gmail.com>
2025-06-18 18:19:59 +0300

    css loaded

--- scripts/helpers/copy-images.sh
+++ scripts/helpers/copy-images.sh
@@ -3,8 +3,9 @@
 folders=$(find . -maxdepth 1 -type d -name 'site-*' -printf '%P\n')
 for folder in $folders; do
     
-    mkdir -p "$folder/img"
-    cp files/img/*.png "$folder/img/"
+    mkdir -p "$folder/static/img"
+    cp files/img/*.png "$folder/static/img/"
+
 
     cp files/logo.jpg "$folder/logo.jpg"
     cp files/favicon.png "$folder/favicon.png"
