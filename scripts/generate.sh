7a15ae509628d952c218f62c436260f0c9024f83
ahmedjw <ahmedvander@gmail.com>
2025-06-18 19:39:44 +0300

    Images loaded

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-bash scripts/helpers/create-css.sh
+bash scripts/helpers/create-css.
+bash scripts/helpers/create-products.sh
 bash scripts/helpers/copy-images.sh
 bash scripts/helpers/create-pages.sh
-bash scripts/helpers/create-products.sh
 bash scripts/helpers/merge-js.sh
 
